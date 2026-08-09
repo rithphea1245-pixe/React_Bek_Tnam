77aaada4f66720719efe732352d709b5b5182e89
Rithz <rithphea1245@gmail.com>
2026-08-10 02:01:20 +0700

    Fix: revert to Firebase default authDomain (no console setup needed) for reliable Google login

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -8,7 +8,7 @@ import { getAuth } from "firebase/auth";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCZJHKMcr9g8m0_dc1XULfrWA4jl9qvyTQ",
-  authDomain: "react-foundation-three.vercel.app",
+  authDomain: "nodejsxfirebase-40ff6.firebaseapp.com",
   projectId: "nodejsxfirebase-40ff6",
   storageBucket: "nodejsxfirebase-40ff6.firebasestorage.app",
   messagingSenderId: "473262434597",
